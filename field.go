88188862917fb5f831158bf4a8ce30b62f87e370
Sergey Matveev <stargrave@stargrave.org>
2021-01-23 22:12:12 +0300

    Remove unnecessary docstrings

--- field.go
+++ field.go
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// GNU recutils'es recfiles parser on pure Go
+// GNU recutils'es recfiles parser/writer on pure Go
 package recfile
 
 type Field struct {
--- r.go
+++ r.go
@@ -15,7 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// GNU recutils'es recfiles parser on pure Go
 package recfile
 
 import (
